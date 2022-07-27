a255902b8e0a0f4aeb06deb5488f376fd7104422
Curtis Sanderson <cw1sander@gmail.com>
2022-07-27 17:19:49 +0200

    added illustrations mobile

--- single-page-design-portfolio/single-page/src/pages/frontpage.js
+++ single-page-design-portfolio/single-page/src/pages/frontpage.js
@@ -2,6 +2,7 @@ import Logo from '../assets/logo.svg';
 import GraphicDesign from '../assets/pattern-graphic-design.svg'
 import UI from '../assets/pattern-ui-ux.svg'
 import Apps from '../assets/pattern-apps.svg'
+import Illustrations from '../assets/pattern-illustrations.svg'    
 import React from 'react';
 import Button from '../components/blackButton.js'
 const FrontPage = () => {
@@ -15,18 +16,24 @@ const FrontPage = () => {
                 <h1 className="text-black text-center text-4xl font-jakartaL">Design solutions made easy</h1>
                 <p className="text-grey text-center text-base font-jakartaM w-8/12 pt-4">With over ten years of experience in various design disciplines, I'm your one-stop shop for your design needs.</p>
             </header>
-            <div className="pt-5 grid grid-rows-5 grid-cols-2 w-11/12">
-                <div className="bg-blue rounded-lg col-start-1 col-end-3 row-start-1 row-end-2 grid grid-rows-2 grid-cols-2">
+            <div className="grid items-end grid-rows-5 grid-cols-2 w-11/12">
+                <div className="mb-2  bg-blue h-5/6 rounded-lg col-start-1 col-end-3 row-start-1 row-end-2 grid grid-rows-2 grid-cols-2">
                     <img className="h-48 pt-6 pl-6 col-start-2 col-end-3" src={GraphicDesign} alt="graphic design"/>
                     <p className="text-white text-center text-2xl font-jakartaL col-start-1 col-end-3 row-start-2 row-end-3 flex items-end justify-start pb-5 pl-5">Graphic Design</p>
                 </div>
-                <div className="h-44 mt-6 rounded-lg col-start-1 col-end-2 row-start-2 row-end-2 mr-3 bg-orange grid grid-rows-2 grid-cols-2">
-                    <img className="pl-2 pt-6 h-20 row-start-1 row-end-1 col-start-2 col-end-3" src={UI} alt="UI/UX"/>
-                    <p className="text-white text-2xl font-jakartaL col-start-1 col-end-2 row-start-2 row-end-2 flex items-end pl-6 pb-6">UI/UX</p>
-                </div>
-                <div className="h-44 mt-6 rounded-lg col-start-2 col-end-3 row-start-2 row-end-2 ml-3 bg-pink grid grid-rows-2 grid-cols-2">
-                <img className="pl-2 pt-6 h-20 row-start-1 row-end-1 col-start-2 col-end-3" src={Apps} alt="UI/UX"/>
-                    <p className="text-white text-2xl font-jakartaL col-start-1 col-end-2 row-start-2 row-end-2 flex items-end pl-6 pb-6">Apps</p>
+                <div className="col-start-1 col-end-3 row-start-2 row-end-2 grid grid-rows-2 grid-cols-2 ">
+                    <div className="h-44 mt-6 rounded-lg col-start-1 col-end-2 row-start-1 row-end-1 mr-3 bg-orange grid grid-rows-2 grid-cols-2">
+                        <img className="pl-2 pt-6 h-20 row-start-1 row-end-1 col-start-2 col-end-3" src={UI} alt="UI/UX"/>
+                        <p className="text-white text-2xl font-jakartaL col-start-1 col-end-2 row-start-2 row-end-2 flex items-end pl-6 pb-6">UI/UX</p>
+                    </div>
+                    <div className="h-44 mt-6 rounded-lg col-start-2 col-end-3 row-start-1 row-end-2 ml-3 bg-pink grid grid-rows-2 grid-cols-2">
+                        <img className="pl-2 pt-6 h-20 row-start-1 row-end-1 col-start-2 col-end-3" src={Apps} alt="UI/UX"/>
+                        <p className="text-white text-2xl font-jakartaL col-start-1 col-end-2 row-start-2 row-end-2 flex items-end pl-6 pb-6">Apps</p>
+                    </div>
+                    <div className="h-40 mt-6 rounded-lg col-start-1 col-end-3 row-start-2 row-end-2 bg-red grid grid-rows-2 grid-cols-2">
+                        <img className="col-start-2 col-end-3 row-start-1 row-end-1 pl-4 pt-6" src={Illustrations} alt="graphics" />
+                        <p className="pl-6 pt-6 col-start-1 col-end-2 row-start-2 row-end-3 text-white text-2xl font-jakartaL">Illustrations</p>
+                    </div>
                 </div>
             </div>
         
