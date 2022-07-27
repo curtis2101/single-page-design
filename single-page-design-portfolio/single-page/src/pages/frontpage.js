69e4754e8e6243686799b94a6e4fd591d2e1b13d
Curtis Sanderson <cw1sander@gmail.com>
2022-07-27 16:11:13 +0200

    added app Graphic for mobile

--- single-page-design-portfolio/single-page/src/pages/frontpage.js
+++ single-page-design-portfolio/single-page/src/pages/frontpage.js
@@ -1,5 +1,7 @@
 import Logo from '../assets/logo.svg';
 import GraphicDesign from '../assets/pattern-graphic-design.svg'
+import UI from '../assets/pattern-ui-ux.svg'
+import Apps from '../assets/pattern-apps.svg'
 import React from 'react';
 import Button from '../components/blackButton.js'
 const FrontPage = () => {
@@ -17,6 +19,14 @@ const FrontPage = () => {
                 <div className="bg-blue rounded-lg col-start-1 col-end-3 row-start-1 row-end-2 grid grid-rows-2 grid-cols-2">
                     <img className="h-48 pt-6 pl-6 col-start-2 col-end-3" src={GraphicDesign} alt="graphic design"/>
                     <p className="text-white text-center text-2xl font-jakartaL col-start-1 col-end-3 row-start-2 row-end-3 flex items-end justify-start pb-5 pl-5">Graphic Design</p>
+                </div>
+                <div className="h-44 mt-6 rounded-lg col-start-1 col-end-2 row-start-2 row-end-2 mr-3 bg-orange grid grid-rows-2 grid-cols-2">
+                    <img className="pl-2 pt-6 h-20 row-start-1 row-end-1 col-start-2 col-end-3" src={UI} alt="UI/UX"/>
+                    <p className="text-white text-2xl font-jakartaL col-start-1 col-end-2 row-start-2 row-end-2 flex items-end pl-6 pb-6">UI/UX</p>
+                </div>
+                <div className="h-44 mt-6 rounded-lg col-start-2 col-end-3 row-start-2 row-end-2 ml-3 bg-pink grid grid-rows-2 grid-cols-2">
+                <img className="pl-2 pt-6 h-20 row-start-1 row-end-1 col-start-2 col-end-3" src={Apps} alt="UI/UX"/>
+                    <p className="text-white text-2xl font-jakartaL col-start-1 col-end-2 row-start-2 row-end-2 flex items-end pl-6 pb-6">Apps</p>
                 </div>
             </div>
         
