d81908464b9e1a6083965fc958097ac86975487a
Curtis Sanderson <cw1sander@gmail.com>
2022-07-28 22:56:35 +0200

    added functionality for carousel image slider

--- single-page-design-portfolio/single-page/src/components/carousel.js
+++ single-page-design-portfolio/single-page/src/components/carousel.js
@@ -7,6 +7,7 @@ import RightBtn from '../assets/icon-arrow-right.svg'
 const Carousel = ({slides}) => {
     const [current,setCurrent] = useState(0);
     const length = slides.length
+    
 
     const nextSlide = () => {
         setCurrent(current === length - 1 ? 0 : current + 1);
@@ -20,23 +21,29 @@ const Carousel = ({slides}) => {
         return null;
     }
 
-    return (
-        <section className='w-4/5 flex flex-col justify-center items-center'>
+        return (    
+            <>
+        <section className='flex justify-center items-center'>
             {ImageData.map((slide, index) => {
                 return (
-                <div className={index === current ? 'opacity-100 duration-1000 transform' : 'opacity-0 duration-1000'} key={index}>
-                    {index === current && (
-                    <img src={slide.image} alt='multiple images' className='mt-16 h-44 w-96 rounded-xl' />
-                    )}
-                </div>
-            );
-        })}
-                <div className="mt-8 flex ">
-                    <img className='p-6 mr-2 top-2/4 bg-black cursor-pointer select-none rounded-full h-16 w-16' src={LeftBtn} onClick={prevSlide} alt="left arrow" />
-                    <img className='p-6 ml-2 top-2/4 bg-black cursor-pointer select-none rounded-full h-16 w-16' src={RightBtn} onClick={nextSlide} alt="right arrow" />
-                </div>
-        </section>
+                    <div className={index === current ? 'opacity-100 duration-1000 transform flex justify-center items-center' : 'opacity-0 duration-1000 flex justify-center items-center'} key={index}>
+                        {index === current && (<>
+                        <img src={ImageData[current <= 1 ? length - 2 : current -1].image} alt='multiple images' className='mt-16 mr-6 h-44 w-42 rounded-xl' />
+                        <img src={slide.image} alt='multiple images' className='mt-16 h-44 w-44 rounded-xl' />
+                        <img src={ImageData[current >= length -1 ? 0:current +1].image} alt='multiple images' className='mt-16 ml-6 h-44 w-42 rounded-xl' />
+                        </>
+                        )}
+                    </div>
     );
+})}
+        </section>
+        <div className="mt-8 flex ">
+            <img className='p-6 mr-2 top-2/4 bg-black cursor-pointer select-none rounded-full h-16 w-16' src={LeftBtn} onClick={prevSlide} alt="left arrow" />
+            <img className='p-6 ml-2 top-2/4 bg-black cursor-pointer select-none rounded-full h-16 w-16' src={RightBtn} onClick={nextSlide} alt="right arrow" />
+        </div>
+        </>
+        
+);
 }
 
 export default Carousel
