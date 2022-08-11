926a07994d731efdb184700c7c4173e334ccc5ac
Curtis Sanderson <cw1sander@gmail.com>
2022-08-11 12:06:23 +0200

    aligned top section for desktop

--- single-page-design-portfolio/single-page/src/pages/frontpage.js
+++ single-page-design-portfolio/single-page/src/pages/frontpage.js
@@ -22,31 +22,31 @@ const FrontPage = () => {
             </nav>
             <header className="flex flex-col items-center justify-items-center pt-8 md:pt-16 ">
                 <h1 className="text-black text-center text-4xl md:text-5xl font-bold font-jakartaL">Design solutions made easy</h1>
-                <p className="text-grey text-center text-base font-medium font-jakartaM w-9/12 pt-4 md:text-lg md:mt-7 ">With over ten years of experience in various design disciplines, I'm your one-stop shop for your design needs.</p>
+                <p className="text-grey text-center text-base font-medium font-jakartaM w-9/12 pt-4 md:text-lg md:mt-7 lg:mt-6 ">With over ten years of experience in various design disciplines, I'm your one-stop shop for your design needs.</p>
             </header>
             <div className="mt-6 md:mt-16 md:w-full grid content-center md:justify-items-start justify-items-center auto-rows-min auto-cols"> {/*main grid div*/}
-                <div className=" bg-blue w-11/12 md:w-10/12 mb-6 max-h-96 rounded-lg row-start-1 row-end-2 col-start-1 col-end-3 md:ml-10 md:col-end-2 grid grid-rows-2 grid-cols-2">
-                    <img className="h-48 pt-6 pl-6 col-start-2 col-end-3" src={GraphicDesign} alt="graphic design"/>
+                <div className=" bg-blue w-11/12 md:w-10/12 lg:w-96 mb-6 max-h-96 rounded-lg row-start-1 row-end-2 lg:ml-32 lg:mr-6 lg:row-end-3 col-start-1 col-end-3 md:ml-10 md:col-end-2 grid grid-rows-2 grid-cols-2">
+                    <img className="h-48 pt-6 pl-6 lg:pl-16 col-start-2 col-end-3" src={GraphicDesign} alt="graphic design"/>
                     <p className="text-white text-center text-2xl font-bold font-jakartaL col-start-1 col-end-3 row-start-2 row-end-3 flex items-end justify-start pb-5 pl-5">Graphic Design</p>
                 </div>
-                    <div className="h-44 rounded-lg col-start-1 col-end-2 row-start-2 row-end-2 md:m-0 md:w-44 md:h-48 md:row-start-1 md:row-end-2 md:col-start-2 md:col-end-3 mr-3 ml-4 bg-orange grid grid-rows-2 grid-cols-2">
+                    <div className="h-44 rounded-lg col-start-1 col-end-2 row-start-2 row-end-2 md:m-0 md:w-44 md:h-48 md:row-start-1 md:row-end-2 md:col-start-2 md:col-end-3 lg:col-end-2 lg:row-start-1 lg:row-end-1 lg:h-52 mr-3 ml-4 bg-orange grid grid-rows-2 grid-cols-2">
                         <img className="pl-2 pt-6 h-20 row-start-1 row-end-1 col-start-2 col-end-3" src={UI} alt="UI/UX"/>
                         <p className="text-white text-2xl font-bold font-jakartaL col-start-1 col-end-2 row-start-2 row-end-2 flex items-end pl-6 pb-6">UI/UX</p>
                     </div>
-                    <div className="h-44 rounded-lg col-start-2 col-end-3 row-start-2 row-end-2 md:m-0 md:h-48 md:w-44 md:mr-4 md:row-start-1 md:row-end-2 md:col-start-3 md:col-end-4 ml-3 mr-4 bg-pink grid grid-rows-2 grid-cols-2">
+                    <div className="h-44 rounded-lg col-start-2 col-end-3 row-start-2 row-end-2 md:m-0 md:h-48 md:w-44 md:mr-4 md:row-start-1 md:row-end-2 md:col-start-3 md:col-end-4 lg:col-start-2 lg:col-end-3 lg:row-start-1 lg:row-end-1 lg:h-52 lg:ml-52 ml-3 mr-4 bg-pink grid grid-rows-2 grid-cols-2">
                         <img className="pl-2 pt-6 h-20 row-start-1 row-end-1 col-start-2 col-end-3" src={Apps} alt="UI/UX"/>
                         <p className="text-white text-2xl font-bold font-jakartaL col-start-1 col-end-2 row-start-2 row-end-2 flex items-end pl-6 pb-6">Apps</p>
                     </div>
-                    <div className="h-40 w-11/12 mt-48 rounded-lg col-start-1 col-end-3 row-start-2 row-end-2 md:w-11/12 md:mt-56 md:col-start-2 md:col-end-4 md:row-start-1 md:row-end-2 bg-red grid grid-rows-2 grid-cols-2">
-                        <img className="col-start-2 col-end-3 row-start-1 row-end-1 pl-4 pt-6" src={Illustrations} alt="graphics" />
+                    <div className="h-40 w-11/12 mt-48 rounded-lg col-start-1 col-end-3 row-start-2 row-end-2 md:w-11/12 md:mt-56 md:col-start-2 md:col-end-4 md:row-start-1 md:row-end-2 lg:-mt-1 lg:col-start-2 lg:col-end-2 lg:row-start-2 lg:h-40 lg:row-end-3 lg:w-96 bg-red grid grid-rows-2 grid-cols-2">
+                        <img className="col-start-2 col-end-3 row-start-1 row-end-1 lg:pl-10 pl-4 pt-6" src={Illustrations} alt="graphics" />
                         <p className="pl-6 pt-6 col-start-1 col-end-2 row-start-2 row-end-3 text-white text-2xl font-bold font-jakartaL">Illustrations</p>
                     </div>
-                    <div className="h-52 w-11/12 mt-6 rounded-lg col-start-1 col-end-3 row-start-3 row-end-3 md:col-start-1 md:h-44 md:ml-10 md:w-10/12 md:col-end-2 md:row-start-2 md:row-end-2 md:mt-0 bg-green grid grid-rows-2 grid-cols-2">
-                        <img className=" col-start-1 col-end-3 row-start-1 row-end-1 pl-40 pt-6" src={Photography} alt="graphics" />
+                    <div className="h-52 w-11/12 mt-6 rounded-lg col-start-1 col-end-3 row-start-3 row-end-3 md:col-start-1 md:h-44 md:ml-10 md:w-10/12 md:col-end-2 md:row-start-2 md:row-end-2 md:mt-0 lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-2 lg:ml-2 lg:mr-44 lg:h-52 lg:w-96 bg-green grid grid-rows-2 grid-cols-2">
+                        <img className=" col-start-1 col-end-3 row-start-1 row-end-1 lg:pl-48 pl-40 pt-6" src={Photography} alt="graphics" />
                         <p className="pl-6 pt-12 md:pt-10 col-start-1 col-end-2 row-start-2 row-end-3 text-white text-2xl font-jakartaL font-bold">Photography</p>
                     </div>
-                    <div className="h-48 w-11/12 mt-64 rounded-lg col-start-1 col-end-3 row-start-3 row-end-4 md:col-start-2 md:mt-0 md:h-44 md:w-11/12 md:col-end-4 md:row-start-2 md:row-end-2 bg-purple grid grid-rows-2 grid-cols-2">
-                        <img className="col-start-2 col-end-3 row-start-1 row-end-1 pl-4 pt-6" src={Motion} alt="graphics" />
+                    <div className="h-48 w-11/12 mt-64 rounded-lg col-start-1 col-end-3 row-start-3 row-end-4 md:col-start-2 md:mt-0 md:h-44 md:w-11/12 md:col-end-4 md:row-start-2 md:row-end-2 lg:col-start-3 lg:col-end-3 lg:row-start-2 lg:row-end-3 lg:h-40 lg:ml-2 lg:-mt-1 lg:w-96 bg-purple grid grid-rows-2 grid-cols-2">
+                        <img className="col-start-2 col-end-3 row-start-1 row-end-1 lg:pl-10 pl-4 pt-6" src={Motion} alt="graphics" />
                         <p className="pl-6 pt-9 col-start-1 col-end-3 row-start-2 row-end-3 text-white text-2xl font-jakartaL font-bold">Motion Graphics</p>
                     </div>
                 </div>
