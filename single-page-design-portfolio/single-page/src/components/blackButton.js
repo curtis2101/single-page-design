7f7b3ab7f5f15550dcc23a3c25cf5a949b122f81
curtis2101 <55060423+curtis2101@users.noreply.github.com>
2022-08-11 18:05:43 +0200

    Delete single-page-design-portfolio/single-page directory

--- single-page-design-portfolio/single-page/src/components/blackButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Button = () => {
-    return ( 
-        <button className="bg-black h-12 w-7/12 md:w-56 md:h-14 rounded-full text-sm md:text-base font-bold font-jakartaM lg:mr-24 lg:hover:bg-blue text-beige ">Free Consultation</button>
-     );
-}
- 
-export default Button;
